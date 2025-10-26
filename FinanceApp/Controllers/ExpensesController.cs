0671df04d3c4b0780e344f21722595e829c76e7c
Pegasus <Pegasus@PEGASUS>
2025-10-26 02:43:47 +0200

    Add project files.

---
+++ FinanceApp/Controllers/ExpensesController.cs
@@ -0,0 +1,47 @@
+﻿using FinanceApp.Data;
+using FinanceApp.Data.Service;
+using FinanceApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceApp.Controllers
+{
+    public class ExpensesController : Controller
+    {
+        private readonly IexpensesService _expensesService;
+
+        public ExpensesController(IexpensesService expensesService)
+        {
+            _expensesService = expensesService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var expenses = await _expensesService.GetAll();
+            return View(expenses);
+        }
+
+        public IActionResult Create() {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Expense expense)
+        {
+            if (ModelState.IsValid)
+            {
+                await _expensesService.Add(expense);
+                return RedirectToAction("Index");
+            }
+            return View(expense);
+        }
+
+        public IActionResult GetChart() 
+        {
+        var data = _expensesService.GetChartData();
+            return Json(data);  
+        
+        }
+
+
+    }
+}
---
+++ FinanceApp/Data/Service/IexpensesService.cs
@@ -0,0 +1,11 @@
+﻿using FinanceApp.Models;
+
+namespace FinanceApp.Data.Service
+{
+    public interface IexpensesService
+    {
+        Task<IEnumerable<Expense>> GetAll();
+        Task Add(Expense expense);
+        IQueryable GetChartData();
+    }
+}
